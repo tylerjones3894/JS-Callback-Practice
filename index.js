2da649ffb6f860bd1e997357128c2d57273c0ecb
Tyler Jones <tylerjones3894@gmail.com>
2022-12-09 14:18:53 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,38 +1,27 @@
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
-// const character = newImage('assets/green-character/static.gif')
-// move(character).to(100, 250)
-
 const character = newImage('assets/green-character/static.gif')
-// let direction = null;
-// let x = 100;
-// let y = 250;
-// move(character).to(100, 250)
 
-function moveCharacter() {
-let direction = null;
-let x = 100;
-let y = 250;
-
-if(direction === 'west'){
-    x = x - 1
-}
-if(direction === 'north'){
-    y = y + 1
-}
-if(direction === 'east'){
-    x = x + 1
-}
-if(direction === 'south'){
-    y = y - 1
-}
-character.style.left = x + 'px'
-character.style.bottom = y + 'px'
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = `assets/green-character/static.gif`
+    }
+    if(direction === 'west'){
+        character.src = `assets/green-character/west.gif`
+    }
+    if(direction === 'north'){
+        character.src = `assets/green-character/north.gif`
+    }
+    if(direction === 'east'){
+        character.src = `assets/green-character/east.gif`
+    }
+    if(direction === 'south'){
+        character.src = `assets/green-character/south.gif`
+    }
 }
 
-setInterval(moveCharacter, 1)
-
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
